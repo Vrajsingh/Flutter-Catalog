60fd6de0f6557d56e793beede139c7d4d8cc7144
VISHAL RAJ <vishalrajput97084@gmail.com>
2022-09-02 17:43:48 +0530

    Day 12 - ListView Builder, List Generate, Card & Asserts

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;  // we are using final because it will not change in future and there will not be any error message
   final String name = "Vishal";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
         appBar: AppBar(
           title: Text("Catalog App"),
         ),
-        body: Container(
-          constraints: BoxConstraints(
-            minHeight: 70,
-            maxHeight: 200,
-            minWidth: 70,
-            maxWidth: 200,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView.builder(
+              itemCount: dummyList.length,
+              itemBuilder: (context, index) {
+                return ItemWidget(item: dummyList[index],);
+              }
           ),
-            color: Colors.green,
-            child: Container(
-              color: Colors.red,
-            )),
+        ),
       drawer: MyDrawer(),
     );
   }
